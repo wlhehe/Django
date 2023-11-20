0565437a8d47eed36074872f343e9c93e15a708b
wlhehe <wlhehe14@gmail.com>
2023-11-21 07:47:19 +0900

    Add files

--- blog/models.py
+++ blog/models.py
@@ -2,22 +2,27 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class Users(AbstractUser):
-    pass
+class User(AbstractUser):
+    id = models.AutoField(primary_key=True, max_length=20)
+    password = models.CharField(max_length=15, unique=True)
+    email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
+    userName = models.CharField(max_length=15)
 
 class Category(models.Model):
-    nameCategory = models.CharField(max_length=15, blank=True)
-    users = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='category')
+    title = models.CharField(max_length=50)
+    content = models.TextField(null=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_mow_add=True)
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField(null=True)
     createdAt = models.DateTimeField(auto_now=True)
     updatedAt = models.DateTimeField(auto_now_add=True)
-    users = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='posts')
+    users = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
 
 class Comment(models.Model):
     substance = models.TextField(null=True)
-    users = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='comments')
+    users = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def post_creat(request):
+    if request.method == "GET":
+        return render(request, 'post_create_form.html')
+    elif request.method == "POST":
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        post = Post.objects.create(title=title, content=content, user=request.user)
+        post.save()
+        context = {
+            'post': post
+        }
+        return render(request, 'post_detail.html', context)
+
+def post_list_view(request):
+    post_list = Post.objects.all()
+    context = {
+        'post_list': post_list,
+    }
+    return render(request, 'post_list.heml', context)
+
+def post_serch_view(request):
+    keyword = request.POST.get('keyword')
+    result = Post.objects.filter(title__icontains=keyword).order_by('-created_at')
+    context = {
+        'keyword': keyword,
+        'result': result
+    }
+    return render(request, 'post_search_result.html', context)
+
+def post_update(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.user == post.user:
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+
+        post.title = title
+        post.content = content
+        post.save()
+
+        return render(request, 'post_detail.html', {'post': post})
+    
+    return HttpResponse("자신의 게시물만 수정할 수 있습니다.")
+
+def post_delete(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.user == post.user:
+        post.delete()
+        return redirect('/post-list-url/')
+    
+    return HttpResponse("자신의 게시물만 삭제할 수 있습니다.")
